aae3ca452ff463b3fa7ce8676a34b42e7dd09965
Reymerson Concepcion <reymerson.concepcion@gmail.com>
2021-03-11 15:49:50 -1000

    Replace list with list_item

--- functions_basics_ii.py
+++ functions_basics_ii.py
@@ -11,9 +11,9 @@ print(countdown(5))
 
 # Print and Return
 
-def print_and_return(list):
-    num1 = list[0]
-    num2 = list[1]
+def print_and_return(list_item):
+    num1 = list_item[0]
+    num2 = list_item[1]
     print(num1)
     return num2
 
@@ -21,22 +21,22 @@ print(print_and_return([1,2]))
 
 # First Plus Length
 
-def first_plus_length(list):
-    return list[0] + len(list)
+def first_plus_length(list_item):
+    return list_item[0] + len(list_item)
 
 print(first_plus_length([1,2,3,4,5]))
 
 # Values Greater
 
-def values_greater_than_second(list):
+def values_greater_than_second(list_item):
     newList = []
     
-    for x in range(len(list)):
-        if len(list) < 2:
+    for x in range(len(list_item)):
+        if len(list_item) < 2:
             return False
         else:
-            if list[x] > list[1]:
-                newList.append(list[x])
+            if list_item[x] > list_item[1]:
+                newList.append(list_item[x])
     
     print(len(newList))
     return newList
@@ -47,10 +47,10 @@ print(values_greater_than_second([5,2,3,2,1,4]))
 # This Length, That Value
 
 def length_and_value(size, value):
-    list = []
-    while(len(list) < size):
-        list.append(value)
-    return list
+    list_item = []
+    while(len(list_item) < size):
+        list_item.append(value)
+    return list_item
 
 print(length_and_value(4,7))
 print(length_and_value(6,2))
